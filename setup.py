3ef7fb5285184cdaf974442f818d580f1d14ccda
Jair Henrique <jair.henrique@gmail.com>
2015-03-05 20:44:27 -0300

    add base structure to run tests

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='drf-pdf',
+    version='0.0.0',
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
---
+++ tests/test_run.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+class TestRun(object):
+
+    def test_should_run_this(self):
+        assert True
