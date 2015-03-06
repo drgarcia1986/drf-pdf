a8d9bbfc485ef3d52c29898d454e625c63d9e7ed
Diego Garcia <drgarcia1986@gmail.com>
2015-03-06 00:30:20 -0300

    Bump version: 0.0.0 → 0.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name='drf-pdf',
-    version='0.0.0',
+    version='0.1.0',
     install_requires=['djangorestframework>=2.4'],
     author='Diego Garcia',
     author_email='drgarcia1986@gmail.com',
