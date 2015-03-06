944a7a8f65a85a864d2009ce80105b78752ce44a
Diego Garcia <drgarcia1986@gmail.com>
2015-03-06 00:13:31 -0300

    add simple example

---
+++ examples/drf_pdf_examples/drf_pdf/renderer.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+from rest_framework.renderers import BaseRenderer
+
+
+class PDFRenderer(BaseRenderer):
+    media_type = 'application/pdf'
+    format = 'pdf'
+    charset = None
+    render_style = 'binary'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        return data
---
+++ examples/drf_pdf_examples/simple/views.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+from StringIO import StringIO
+
+from rest_framework import status
+from rest_framework.views import APIView
+
+from drf_pdf.renderer import PDFRenderer
+from drf_pdf.response import PDFResponse
+
+from reportlab.pdfgen import canvas
+
+
+class SimpleExample(APIView):
+
+    renderer_classes = (PDFRenderer, )
+
+    def get(self, request):
+        pdf = StringIO()
+
+        gen_pdf = canvas.Canvas(pdf)
+        gen_pdf.drawString(100, 100, 'Simple example')
+        gen_pdf.showPage()
+        gen_pdf.save()
+
+        return PDFResponse(
+            pdf.getvalue(),
+            file_name='example.pdf',
+            status=status.HTTP_200_OK
+        )
