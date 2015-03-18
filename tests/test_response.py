6da6b92b761f3078fb2fa27048ae7ee56d1e0722
Diego Garcia <drgarcia1986@gmail.com>
2015-03-18 00:03:10 -0300

    add feature to response pdf by file name (resolve #12)

--- tests/test_response.py
+++ tests/test_response.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
-from drf_pdf import response
 import codecs
+import os
+import pytest
+from drf_pdf import response, exceptions
 
 
 class TestResponse(object):
     """Test for DRF-PDF Response"""
 
     def test_response(self, pdf_file_sample):
-
-        with codecs.open(pdf_file_sample, 'r', 'ISO-8859-2') as f:
+        with codecs.open(pdf_file_sample, 'rb', 'ISO-8859-2') as f:
             file_data = f.read()
             ret = response.PDFResponse(
                 file_data,
@@ -18,3 +19,18 @@ class TestResponse(object):
         assert ret.content_type.lower() == 'application/pdf'
         assert ret['Content-Length'] == str(len(file_data))
         assert ret['Content-Disposition'].lower() == 'filename="sample.pdf"'
+
+
+class TestFileResponse(object):
+    """Test for DRF-PDF File Response"""
+
+    def test_response_by_file_name(self, pdf_file_sample):
+        ret = response.PDFFileResponse(file_path=pdf_file_sample)
+
+        assert ret.content_type.lower() == 'application/pdf'
+        assert ret['Content-Disposition'].lower() == \
+            'filename="{}"'.format(os.path.basename(pdf_file_sample))
+
+    def test_response_file_not_found(self, pdf_file_sample):
+        with pytest.raises(exceptions.PDFFileNotFound):
+            response.PDFFileResponse(file_path='foo.bar')
