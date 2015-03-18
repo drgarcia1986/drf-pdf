b6476ebc89a2b6c5285f57bdc281d4a9606fefe1
Renan Ivo <renanivom@gmail.com>
2015-03-17 22:57:21 -0300

    create docstrings in PDFRenderer and PDFResponse

--- drf_pdf/response.py
+++ drf_pdf/response.py
@@ -3,6 +3,14 @@ from rest_framework.response import Response
 
 
 class PDFResponse(Response):
+    """
+    DRF Response to render data as a PDF File
+
+    kwargs:
+        - pdf (byte array). The PDF file content.
+        - file_name (string). The default downloaded file name.
+    """
+
     def __init__(self, pdf, file_name, *args, **kwargs):
         headers = headers = {
             'Content-Disposition': 'filename="{}.pdf"'.format(file_name),
